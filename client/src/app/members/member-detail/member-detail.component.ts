b3581f496506b2d39d63c0dce7494ce8aafd94e0
zvi naiman <zvishmuel@gmail.com>
2024-09-06 12:28:05 +0300

    section 9 +gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,12 +3,13 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule,GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -20,6 +21,7 @@ ngOnInit(): void {
 private memberService = inject(MembersService);
 private route = inject(ActivatedRoute);
 member?:Member;
+images:GalleryItem[]=[];
 
 loadMember(){
   const username = this.route.snapshot.paramMap.get('username');
@@ -27,7 +29,13 @@ loadMember(){
     return;
 
   this.memberService.getMember(username).subscribe({
-    next: serever_member => this.member=serever_member
+    next: serever_member =>{
+    this.member=serever_member;
+    serever_member.photos.map(p=>{
+      this.images.push(new ImageItem({src:p.url, thumb:p.url}));
+    })
+
+    }
   });
 }
 
